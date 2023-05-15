9263fde390215ffd4d3ffc451545c0e695a3e89e
mart cube <martingjorceski@gmail.com>
2023-05-15 21:12:06 +0200

    folder for each storyblok section

---
+++ src/storyblok/SectionBlog/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const articleNumber = 3
+const { data: articles, pending } = await useAsyncGql("LatestArticles", { articleNumber })
+</script>
+
+<template>
+	<section class="section-blog">
+		<template v-if="articles && !pending">
+			<div class="grid">
+				<ArticleCard v-for="item in articles.ArticleItems?.items" :data="item!" />
+			</div>
+		</template>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.section-blog {
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+
+	.grid {
+		width: 100%;
+		display: flex;
+		justify-content: space-evenly;
+
+	}
+}
+</style>
---
+++ src/storyblok/SectionRealty/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+
+const projectNumber = 6
+const { data: projects, pending } = await useAsyncGql("LatestProjects", { projectNumber })
+</script>
+
+<template>
+	<section class="section-realty">
+		<template v-if="projects && !pending">
+			<div class="grid">
+				<ProjectCard v-for="item in projects.ProjectItems?.items" :data="item!" />
+			</div>
+		</template>
+	</section>
+</template>
+
+
+<style lang="scss" scoped>
+.section-realty {
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+
+	.grid {
+		width: 100%;
+		display: flex;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+	}
+}
+</style>
