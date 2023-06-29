21fd62ea1089249e29148289706e1d4e5625f785
mart cube <martingjorceski@gmail.com>
2023-06-29 12:38:55 +0200

    fixed load more btn

--- src/storyblok/SectionBlog/index.vue
+++ src/storyblok/SectionBlog/index.vue
@@ -26,8 +26,8 @@ async function loadMore() {
 	<section class="section-blog mb-40 min-h-screen">
 		<div v-if="articles && !pending" class="container m-auto px-5 lg:px-0 max-w-[970px]">
 			<div class="md:grid md:grid-cols-2 md:gap-7 artcle-wrap">
-					<!-- <ArticleCard v-for="item in articles.ArticleItems?.items" :data="item!" /> -->
-					<ArticleCard v-for="item in newArr" :data="item!" />
+				<!-- <ArticleCard v-for="item in articles.ArticleItems?.items" :data="item!" /> -->
+				<ArticleCard v-for="item in newArr" :data="item!" />
 			</div>
 			<!-- <div
         class="md:grid md:grid-cols-2 md:gap-7 artcle-wrap"
@@ -37,35 +37,37 @@ async function loadMore() {
           <ArticleCard v-for="item in newArr" :data="item!" />
         </transition-group>
       </div> -->
-			<div class="mt-20" v-if="articles.ArticleItems?.total >= articleNumberTest">
-					<button @click="loadMore" class="swipe-btn">
-						<div class="swipe-btn__content">
-							<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M15 16C15.2833 16 15.521 15.904 15.713 15.712C15.905 15.52 16.0007 15.2827 16 15V6C16 5.71667 15.904 5.479 15.712 5.287C15.52 5.095 15.2827 4.99934 15 5C14.7167 5 14.479 5.096 14.287 5.288C14.095 5.48 13.9993 5.71734 14 6V14H6C5.71666 14 5.479 14.096 5.287 14.288C5.095 14.48 4.99933 14.7173 5 15C5 15.2833 5.096 15.521 5.288 15.713C5.48 15.905 5.71733 16.0007 6 16H15ZM10 11C10.2833 11 10.521 10.904 10.713 10.712C10.905 10.52 11.0007 10.2827 11 10V1C11 0.71667 10.904 0.479004 10.712 0.287004C10.52 0.0950036 10.2827 -0.000663206 10 3.4602e-06C9.71666 3.4602e-06 9.479 0.0960036 9.287 0.288004C9.095 0.480004 8.99933 0.717337 9 1V9H0.999996C0.716662 9 0.478997 9.096 0.286997 9.288C0.0949965 9.48 -0.000671387 9.71734 -3.8147e-06 10C-3.8147e-06 10.2833 0.0959959 10.521 0.287996 10.713C0.479997 10.905 0.71733 11.0007 0.999996 11H10Z" fill="#FDF6E9"/>
-							</svg>
-								<span class="whitespace-nowrap">See more updates</span>
-						</div>
-					</button>
-				</div>
+			<div class="mt-20" v-if="articles.ArticleItems!!.total!! >= articleNumberTest">
+				<button @click="loadMore" class="swipe-btn">
+					<div class="swipe-btn__content">
+						<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M15 16C15.2833 16 15.521 15.904 15.713 15.712C15.905 15.52 16.0007 15.2827 16 15V6C16 5.71667 15.904 5.479 15.712 5.287C15.52 5.095 15.2827 4.99934 15 5C14.7167 5 14.479 5.096 14.287 5.288C14.095 5.48 13.9993 5.71734 14 6V14H6C5.71666 14 5.479 14.096 5.287 14.288C5.095 14.48 4.99933 14.7173 5 15C5 15.2833 5.096 15.521 5.288 15.713C5.48 15.905 5.71733 16.0007 6 16H15ZM10 11C10.2833 11 10.521 10.904 10.713 10.712C10.905 10.52 11.0007 10.2827 11 10V1C11 0.71667 10.904 0.479004 10.712 0.287004C10.52 0.0950036 10.2827 -0.000663206 10 3.4602e-06C9.71666 3.4602e-06 9.479 0.0960036 9.287 0.288004C9.095 0.480004 8.99933 0.717337 9 1V9H0.999996C0.716662 9 0.478997 9.096 0.286997 9.288C0.0949965 9.48 -0.000671387 9.71734 -3.8147e-06 10C-3.8147e-06 10.2833 0.0959959 10.521 0.287996 10.713C0.479997 10.905 0.71733 11.0007 0.999996 11H10Z" fill="#FDF6E9" />
+						</svg>
+						<span class="whitespace-nowrap">See more updates</span>
+					</div>
+				</button>
+			</div>
 		</div>
 	</section>
 </template>
 
 <style lang="scss" scoped>
-
 .post-list {
-  display: inline-block;
-  margin-right: 10px;
+	display: inline-block;
+	margin-right: 10px;
 }
+
 .post-list-enter-active,
 .post-list-leave-active {
-  transition: all 1s ease;
+	transition: all 1s ease;
 }
+
 .post-list-enter-from,
 .post-list-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
+	opacity: 0;
+	transform: translateY(30px);
 }
+
 .section-blog {
 	padding: 20px 0;
 }
@@ -95,24 +97,27 @@ async function loadMore() {
 	transition: all 1.1s;
 
 	&__content {
-			display: flex;
-			align-items: center;
-			position: relative;
-			height: 100%;
-			width: 100%;
-			justify-content: center;
-			span {
-				margin-left: 10px;
-				transition: .5s all;
-			}
+		display: flex;
+		align-items: center;
+		position: relative;
+		height: 100%;
+		width: 100%;
+		justify-content: center;
+
+		span {
+			margin-left: 10px;
+			transition: .5s all;
 		}
+	}
 
 	@media (min-width:760px) {
 		max-width: 70px;
 		left: 65%;
+
 		&__content {
 			width: auto;
 			justify-content: unset;
+
 			span {
 				opacity: 0;
 			}
@@ -126,6 +131,7 @@ async function loadMore() {
 				opacity: 1;
 			}
 		}
+
 		@keyframes bounce-back {
 			0% {
 				max-width: 220px;
@@ -151,5 +157,4 @@ async function loadMore() {
 		}
 	}
 
-}
-</style>
+}</style>
