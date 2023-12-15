fed07e4554a1199ce2b43fab562b58e1a2090d7e
@Akirienko <lakirienkol@gmail.com>
2023-12-15 09:40:31 +0200

    added seo tag for crm

--- src/app.vue
+++ src/app.vue
@@ -5,15 +5,23 @@ const story: Ref<AppData> = await useAsyncStoryblok("app", {
 	version: "published",
 })
 
+useHead({
+  script: [
+    {
+      src: 'https://crm.livrichy.com/upload/crm/tag/call.tracker.js',
+      // valid options are: 'head' | 'bodyClose' | 'bodyOpen'
+      tagPosition: 'bodyClose'
+    }
+  ]
+})
+
 useProject()
 </script>
 
 <template>
 	<div class="app mt-20 lg:mt-24">
 		<AppHeader :data="story" />
-		<!-- <NuxtLayout> -->
 			<NuxtPage />
-		<!-- </NuxtLayout> -->
 		<AppFooter :data="story" />
 	</div>
 </template>
